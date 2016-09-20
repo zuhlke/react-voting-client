ba2e258c78ef17600d6e78b0d4c0d72dcff493b4
Lachezar Kozhuharov <lachezar.kozhuharov@zuhlke.com>
2016-09-20 02:46:32 +0200

    Results screen: display winner and allow next vote to be triggered

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Winner from './Winner'
 
 export default class Results extends Component {
   getPair() {
@@ -12,15 +13,26 @@ export default class Results extends Component {
   }
 
   render() {
-    return <div className="results">
-      {this.getPair().map(entry =>
-        <div key={entry} className="entry">
-          <h1>{entry}</h1>
-          <div className="voteCount">
-            {this.getVotes(entry)}
-          </div>
+    return this.props.winner ?
+      <Winner ref="winner" winner={this.props.winner} /> :
+       <div className="results">
+        <div className="tally">
+          {this.getPair().map(entry =>
+            <div key={entry} className="entry">
+              <h1>{entry}</h1>
+              <div className="voteCount">
+                {this.getVotes(entry)}
+              </div>
+            </div>
+          )}
         </div>
-      )}
-    </div>
+        <div className="management">
+          <button ref="next"
+                  className="next"
+                  onClick={this.props.next}>
+            Next
+          </button>
+        </div>
+      </div>
   }
 }
