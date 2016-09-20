b267fc82ec2267e7d0f5ccf9dc33fd5ce2dd238a
Lachezar Kozhuharov <lachezar.kozhuharov@zuhlke.com>
2016-09-20 07:55:14 +0200

    Add socket.io client

--- src/index.js
+++ src/index.js
@@ -5,22 +5,17 @@ import { Router, Route, hashHistory } from 'react-router'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducer'
+import io from 'socket.io-client'
 import App from './components/App'
 import Voting from './components/Voting'
 import Results from './components/Results'
 
 const store = createStore(reducer)
-store.dispatch({
-  type: 'SET_STATE',
-  payload: {
-    state: {
-      vote: {
-        pair: ['sublime', 'emacs'],
-        tally: {sublime: 2}
-      }
-    }
-  }
-})
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`)
+socket.on('state', state =>
+  store.dispatch({type: 'SET_STATE', state})
+)
 
 const appElement = document.getElementById('app')
 const renderApp = () => {
