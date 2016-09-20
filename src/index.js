7dcb52b200b0473394964279127592581590304b
Lachezar Kozhuharov <lachezar.kozhuharov@zuhlke.com>
2016-09-20 03:57:33 +0200

    Add redux store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import { render, unmountComponentAtNode } from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
-import { Router, Route, hashHistory } from 'react-router';
-import App from './components/App';
+import { Router, Route, hashHistory } from 'react-router'
+import {createStore} from 'redux'
+import reducer from './reducer'
+import App from './components/App'
 import Voting from './components/Voting'
 import Results from './components/Results'
 
-const pair = ['sublime', 'emacs']
+const store = createStore(reducer)
+store.dispatch({
+  type: 'SET_STATE',
+  payload: {
+    state: {
+      vote: {
+        pair: ['sublime', 'emacs'],
+        tally: {sublime: 2}
+      }
+    }
+  }
+})
 
 const appElement = document.getElementById('app')
 const renderApp = () => {
   const routes = <Route component={App}>
     <Route path="/" component={Voting} />
     <Route path="/results" component={Results} />
-  </Route>;
+  </Route>
 
   render(
     <AppContainer>
@@ -28,7 +41,7 @@ renderApp()
 if (module.hot) {
   module.hot.accept('./components/App', () => {
     // Prevent the hot reloading error from react-router
-    unmountComponentAtNode(appElement);
-    renderApp();
-  });
+    unmountComponentAtNode(appElement)
+    renderApp()
+  })
 }
