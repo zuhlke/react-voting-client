fe63d16b3480f4c4eadf7d7e5103529a1c9c31b1
Lachezar Kozhuharov <lachezar.kozhuharov@zuhlke.com>
2016-09-20 02:13:25 +0200

    Add implementation for results screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
 
 const pair = ['sublime', 'emacs']
+const tally = {'sublime': 5, 'emacs': 4}
 
 export default class App extends Component {
   render() {
-    return React.cloneElement(this.props.children, {pair: pair})
+    return React.cloneElement(this.props.children, {pair, tally})
   }
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,26 @@
 import React, {Component} from 'react'
 
 export default class Results extends Component {
+  getPair() {
+    return this.props.pair || []
+  }
+  getVotes(entry) {
+    if (this.props.tally && this.props.tally[entry]) {
+      return this.props.tally[entry];
+    }
+    return 0;
+  }
+
   render() {
-    return <div>Hello from results!</div>
+    return <div className="results">
+      {this.getPair().map(entry =>
+        <div key={entry} className="entry">
+          <h1>{entry}</h1>
+          <div className="voteCount">
+            {this.getVotes(entry)}
+          </div>
+        </div>
+      )}
+    </div>
   }
 }
