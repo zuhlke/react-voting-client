77f53887f3fa590545de445f48cfdde366b4d274
Lachezar Kozhuharov <lachezar.kozhuharov@zuhlke.com>
2016-09-20 08:18:41 +0200

    Add hasVoted implementation

--- src/reducer.js
+++ src/reducer.js
@@ -2,10 +2,23 @@ const setState = (state, newState) => {
   return Object.assign({}, state, newState)
 }
 
+const vote = (state, entry) => {
+  if (state.vote && state.vote.pair && state.vote.pair.indexOf(entry) > -1) {
+    return {
+      ...state,
+      hasVoted: entry
+    }
+  } else {
+    return state;
+  }
+}
+
 const reducer = (state = {}, {type, payload}) => {
   switch (type) {
-  case 'SET_STATE':
-    return setState(state, payload.state)
+    case 'SET_STATE':
+      return setState(state, payload.state)
+    case 'VOTE':
+      return vote(state, payload.entry);
   }
   return state
 }
